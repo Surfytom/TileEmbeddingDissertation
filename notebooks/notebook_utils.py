1ce4ed0e2eab6d5d2b77732345f1af5189a9efaf
Surfytom <surfytom@gmail.com>
2024-09-11 18:27:33 +0100

    removed uneeded files from forked repositoy

--- notebooks/notebook_utils.py
+++
@@ -1,8 +0,0 @@
-import sys
-from pathlib import Path
-
-
-def initialize_environment():
-    BASE_DIR = str(Path("./../src/").resolve())
-    if BASE_DIR not in sys.path:
-        sys.path.append(BASE_DIR)
